34a29f24624555a996e43348474968668371345e
saimohan11 <saimohan11sai@gmail.com>
2025-01-09 10:48:45 +0530

    created server and configured mongodb

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import main_router from './routes/index.js'
+import cors from 'cors'
+const app = express();
+const port = 3000;
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+.then(async ()=>{
+    console.log("mongo db connected")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.use(express.json());
+
+app.use("/api/v1",main_router)
+
+mongoose.connection.on("open",()=>{
+    app.listen(port,()=>{
+        console.log(`server is running on http://localhost:${port}`)
+    })
+})
