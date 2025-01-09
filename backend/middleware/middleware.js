97e3b6c7e4b875077823873d63b4c39408c2dd0d
saimohan11 <saimohan11sai@gmail.com>
2025-01-09 10:47:31 +0530

    added user middleware

---
+++ backend/middleware/middleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import JWT_SECRET from '../config/secret.js'
+
+const authMiddleWare = (req,res,next)=>{
+    const authHeader = req.header.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({
+            error: "No Auth header found"
+        })
+    }
+    const token = authHeader.split(" ")[1]
+    const decoded = jwt.verify(token,JWT_SECRET);
+    if(decoded.userId) {
+        req.userId = decoded.userId;
+        console.log(req.userId)
+        next()
+    } else {
+        return res.status(403).json({
+            error: "Invalid token"
+        })
+    }
+}
