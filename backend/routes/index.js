544c77c735e6df9e4cdbbbcd77cb18a39d4e688b
saimohan11 <saimohan11sai@gmail.com>
2025-01-10 01:03:11 +0530

    added signin route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
 import Routes from 'express'
 // import user_route from './user_route.js'
-import { signup } from '../controllers/user.js';
+import { signin, signup } from '../controllers/user.js';
 const router = Routes()
 
 router.post('/user', signup);
+router.post('/signin',signin)
 
 export default router;
