0cb0eec8c0c7f72c0ada28cbc023265e1424ca41
saimohan11 <saimohan11sai@gmail.com>
2025-01-15 23:30:23 +0530

    added signin and signup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
-      
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/signin" element={<Signin/>} /> 
     </Routes>
     </BrowserRouter>
   )
