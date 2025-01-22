eb2000df7f60cc850dd8085a5b7a22ce2c35f3fb
saimohan11 <saimohan11sai@gmail.com>
2025-01-22 08:31:41 +0530

    added bulk data route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 import Routes from 'express'
 // import user_route from './user_route.js'
-import { signin, signup, updateUser } from '../controllers/user.js';
+import { getUser, signin, signup, updateUser } from '../controllers/user.js';
 import { authMiddleWare } from '../middleware/middleware.js';
 import { balance, transfer } from '../controllers/account.js';
 
@@ -11,5 +11,6 @@ router.post('/signin',signin);
 router.put('/',authMiddleWare,updateUser); //update
 router.get('/balance',authMiddleWare,balance);
 router.post('/transfer',authMiddleWare,transfer);
+router.get("/bulk",getUser);
 
 export default router;
