42673c4677f01cbd6decd38224dd95a46f0c0874
saimohan11 <saimohan11sai@gmail.com>
2025-01-24 11:33:58 +0530

    added new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
+// import { Navbar } from "./pages/Navbar";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
 export default function App() {
   return (
+    <>
     <BrowserRouter>
     <Routes>
+      <Route path="/dashboard" element={<Dashboard/>} />
       <Route path="/signup" element={<Signup/>} />
       <Route path="/signin" element={<Signin/>} /> 
+      <Route path="/send" element={<SendMoney/>} />
     </Routes>
     </BrowserRouter>
+    </>
   )
 }
